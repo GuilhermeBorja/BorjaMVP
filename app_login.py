97555d9b27c31fe9f4205e8833cd08fe38632a47
GuilhermeBorja <104537034+GuilhermeBorja@users.noreply.github.com>
2025-04-29 14:45:44 -0300

    Update app_login.py

--- app_login.py
+++ app_login.py
@@ -1,22 +1,20 @@
 import streamlit as st
 from db_connect import get_connection
 
-
 def login():
     container = st.container()
     with container:
         st.header("Login")
-        user = st.text_input("Usuário", key="login_username")
-        pwd = st.text_input("Senha", type="password", key="login_password")
-        if st.button("Entrar", key="btn_entrar"):
-            conn = get_connection()
-            cur = conn.cursor()
-            cur.execute("SELECT * FROM users WHERE username=? AND password=?", (user, pwd))
-            record = cur.fetchone()
+        username = st.text_input("Usuário", key="login_user")
+        password = st.text_input("Senha", type="password", key="login_pwd")
+        if st.button("Entrar", key="login_btn"):
+            conn = get_connection(); cur = conn.cursor()
+            cur.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+            user = cur.fetchone()
             conn.close()
-            if record:
-                st.success("Login bem‑sucedido!")
-                st.session_state.user = dict(record)
+            if user:
+                st.success("Login bem-sucedido!")
+                st.session_state.user = dict(user)
                 container.empty()
             else:
                 st.error("Usuário ou senha inválidos.")
